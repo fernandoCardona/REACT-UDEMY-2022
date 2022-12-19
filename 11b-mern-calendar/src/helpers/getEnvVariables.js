43aeb664a9303954967eee2d45a4a9548923c1c2
Fernando Cardona <fernando.cardona@the-lion.com>
2022-12-19 13:39:59 +0100

    rutas .env.production

--- 11b-mern-calendar/src/helpers/getEnvVariables.js
+++ 11b-mern-calendar/src/helpers/getEnvVariables.js
@@ -5,10 +5,11 @@
 export const getEnvVariables = () => {
 
     //17.0-Importamos nuestras variables de entorno:
-    import.meta.env;
+    //import.meta.env;
 
     return {
-        ...import.meta.env
+        //...import.meta.env
+        VITE_API_URL: import.meta.env.VITE_API_URL
     }
 }
 
